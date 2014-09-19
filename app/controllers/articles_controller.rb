0a55747158b4c27b2efb2ddc3351b6df2761b089
Surrexi <surrexi@surrexi-K43SD.(none)>
2014-09-19 23:07:23 +0600

    test3

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,38 +8,35 @@ class ArticlesController < ApplicationController
 	end
 
 	def new
-    @article = Article.new
+		@article = Article.new
 	end
 
 	def edit
-    @article = Article.find(params[:id])
+		@article = Article.find(params[:id])
 	end
 
 	def create
 		@article = Article.new(article_params)
-
 		if @article.save
-		  redirect_to @article
-    else
-      render 'new'
-    end
+			redirect_to @article
+		else
+			render 'new'
+		end
 	end
 
 	def update
-    @article = Article.find(params[:id])
-		
-    if @article.update(article_params)
-      redirect_to @article
-    else
-      render 'edit'
-    end
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
-    @article = Article.find(params[:id])
-    @article.destroy
-
-    redirect_to articles_path
+		@article = Article.find(params[:id])
+		@article.destroy
+		redirect_to articles_path
 	end
 
 	private
